d4af308c87571baf3e929083c67e60e8caf86af2
sacemakesgame <mungkinchristian@apps.ipb.ac.id>
2025-05-15 19:57:56 +0700

    change port

--- index.js
+++ index.js
@@ -325,4 +325,5 @@ app.delete('/jadwal/:id', (req, res) => {
   );
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.DB_PORT
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
